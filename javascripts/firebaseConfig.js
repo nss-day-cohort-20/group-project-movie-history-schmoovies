5550da832b1e29888ac624b29bebe843dd0b83c3
Arwa <arwa.it15@gmail.com>
2017-07-18 17:16:32 -0500

    creating of basic js files

---
+++ javascripts/firebaseConfig.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let firebase = require("firebase");
+let fbData = require('./fb-getter')(); //self invoke as it returns an unknown function
+
+//make fbData - so that we dont push up our api keys to github and make it publicly available
+
+require('firebase/auth');
+
+let config = {
+	apiKey: fbData.key,
+	authDomain: fbData.authDomain
+};
+
+firebase.initializeApp(config);
+
+module.exports = firebase;

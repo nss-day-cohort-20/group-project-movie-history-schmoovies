8207da64a1915537e371a9418a600d8315a4e420
thejonroberts <jon.roberts.8@gmail.com>
2017-07-18 17:50:45 -0500

    setup bootstrap scripts, made very basic page layout

---
+++ javascripts/main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+let $ = require('jquery');
+window.jQuery = require("jquery");
+let Handlebars = require('hbsfy/runtime');
+let bootstrap = require('../lib/node_modules/bootstrap/dist/js/bootstrap.min.js');

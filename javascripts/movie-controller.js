c0d9c839818585bbb9ad844d4917a591a6ec0221
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-07-18 22:45:41 -0500

    placeholders for functions)

---
+++ javascripts/movie-controller.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let $ = require('jquery');
+let db = require('./movie-factory');
+let templates = require('./template-builder');
+let $container = $('#movieContainer');
+
+module.exports.loadMoviesToDOM = () => {
+
+};
+
+module.exports.setupEvents = () => {
+
+	//various handlers
+
+};
---
+++ javascripts/movie-factory.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let $ = require('jquery');
+let fbURL = 'https://schmoovies-e903e.firebaseapp.com';
+let firebase = require('./firebaseConfig');
+
+module.exports.newMoviesSearch = (searchString) => {
+
+};
+
+module.exports.savedMoviesSearch = (searchString) => {
+
+};
+
+module.exports.getUnWatchedMovies = () => {
+
+};
+
+module.exports.getWatchedMovides = () => {
+
+};

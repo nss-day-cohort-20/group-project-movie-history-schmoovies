6576850c0f5716c11659d91fa64dc603ded6ee0c
thejonroberts <jon.roberts.8@gmail.com>
2017-07-19 14:38:43 -0500

    move function to movie view controller

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,19 +6,21 @@ let Handlebars = require('hbsfy/runtime');
 let bootstrap = require('../lib/node_modules/bootstrap/dist/js/bootstrap.min.js');
 require('./user-factory'); // user-factory makes sigin signout functions and runs
 
-//templates import
-Handlebars.registerPartial( "movieInfoPartial", require('../templates/partials/movie-info.hbs') );
-let searchCardTemplate = require ('../templates/searchCards.hbs');
+
+
+
+
+
 
 //call for example 'BATMAN' search in data folder
-$.ajax({
-	  url: `../data/search-results-batman.json`
-		})
-		.done( (data) => {
-			console.log('data', data);
-			let cards = searchCardTemplate({movies: data.results});
-			$("#movieContainer").append(cards);
-		})
-		.fail( (reject) => {
-			console.log('error in ajax');
-		});
+// $.ajax({
+// 	  url: `../data/search-results-batman.json`
+// 		})
+// 		.done( (data) => {
+// 			console.log('data', data);
+// 			let cards = searchCardTemplate({movies: data.results});
+// 			$("#movieContainer").append(cards);
+// 		})
+// 		.fail( (reject) => {
+// 			console.log('error in ajax');
+// 		});

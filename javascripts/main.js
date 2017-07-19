39e3e361887b3c811da9ff836092a467d83b72ea
thejonroberts <jon.roberts.8@gmail.com>
2017-07-19 14:31:19 -0500

    add some comments

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,9 +6,11 @@ let Handlebars = require('hbsfy/runtime');
 let bootstrap = require('../lib/node_modules/bootstrap/dist/js/bootstrap.min.js');
 require('./user-factory'); // user-factory makes sigin signout functions and runs
 
+//templates import
 Handlebars.registerPartial( "movieInfoPartial", require('../templates/partials/movie-info.hbs') );
 let searchCardTemplate = require ('../templates/searchCards.hbs');
 
+//call for example 'BATMAN' search in data folder
 $.ajax({
 	  url: `../data/search-results-batman.json`
 		})

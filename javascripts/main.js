39df78c650d48a8dfd8d9c2dad334cd59063d930
Arwa <arwa.it15@gmail.com>
2017-07-19 13:21:36 -0500

    moving login logout to user-factory + movie-info partial

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,35 +4,5 @@ let $ = require('jquery');
 window.jQuery = require("jquery");
 let Handlebars = require('hbsfy/runtime');
 let bootstrap = require('../lib/node_modules/bootstrap/dist/js/bootstrap.min.js');
-let userFactory = require('./user-factory');
-// songController.loadSongsToDom();
-// on click on sign in button.
-$("#auth-btn").click( () =>
-{
-	console.log("auth-btn-clicked");
-	$('#auth-btn').toggleClass('isHidden');
-	$('#unauth-btn').toggleClass('isHidden');
-	userFactory.logInGoogle()
-	.then( (result) => {
-		let user = result.user.uid;
-		console.log('user', user);
-		// do something with user here.
-	}).catch( (err) => {
-		console.log('error signing in', err);
-	});
-});
+require('./user-factory'); // user-factory makes sigin signout functions and runs
 
-// on click on logout button. make sure it has isHidden class.
-$("#unauth-btn").click( () =>
-{
-	console.log('unauth-btn clicked');
-	userFactory.logOut()
-	.then( (result) => {
-		console.log('Successfully signed out');
-		location.reload();
-	})
-	.catch( (err) =>
-	{
-		console.log('error signing you out', err);
-	});
-});
--- javascripts/user-factory.js
+++ javascripts/user-factory.js
@@ -1,14 +1,45 @@
 'use strict';
 
+let $ = require('jquery');
 let firebase = require('./firebaseConfig');
 let provider = new firebase.auth.GoogleAuthProvider();
-module.exports.logInGoogle = () =>
+let logInGoogle = () =>
 {
 	console.log("in auth");
 	return firebase.auth().signInWithPopup(provider);
 };
 
-module.exports.logOut = () =>
+let logOut = () =>
 {
 	return firebase.auth().signOut();
 };
+
+$("#auth-btn").click( () =>
+{
+	console.log("auth-btn-clicked");
+	$('#auth-btn').toggleClass('isHidden');
+	$('#unauth-btn').toggleClass('isHidden');
+	logInGoogle()
+	.then( (result) => {
+		let user = result.user.uid;
+		console.log('user', user);
+		// do something with user here.
+	}).catch( (err) => {
+		console.log('error signing in', err);
+	});
+});
+
+// on click on logout button. make sure it has isHidden class.
+$("#unauth-btn").click( () =>
+{
+	console.log('unauth-btn clicked');
+	logOut()
+	.then( (result) => {
+		console.log('Successfully signed out');
+		location.reload();
+	})
+	.catch( (err) =>
+	{
+		console.log('error signing you out', err);
+	});
+});

29be9cae53e7cd153de213d9b64175cc707530f5
thejonroberts <jon.roberts.8@gmail.com>
2017-07-20 13:30:45 -0500

    draw saved movies to dom - will be changed to new system, but working

--- javascripts/movie-view-controller.js
+++ javascripts/movie-view-controller.js
@@ -1,14 +1,24 @@
 'use strict';
 
 let $ = require('jquery');
-let Handlebars = require('hbsfy/runtime');
-let fbURL = "https://schmoovies-e903e.firebaseio.com";
 
+let Handlebars = require('hbsfy/runtime');
 Handlebars.registerPartial( "movieInfoPartial", require('../templates/partials/movie-info.hbs') );
 let searchCardTemplate = require ('../templates/searchCards.hbs');
+let unwatchedCardTemplate = require ('../templates/unwatchedCard.hbs');
+
+let fbFactory = require('./firebase-factory');
+let fbURL = "https://schmoovies-e903e.firebaseio.com";
+
 
 module.exports.searchDataToMovieCards = (data) => {
 	let cards = searchCardTemplate({movies: data.results});
+	$("#movieContainer").append(cards);
+};
+
+module.exports.unwatchedFBToMovieCards = (data) => {
+	console.log('user movies from FB', data);
+	let cards = unwatchedCardTemplate({movies: data});
 	$("#movieContainer").html(cards);
 };
 
@@ -27,3 +37,19 @@ module.exports.deleteFromScreen = (movieObjId) => {
 	    });
 	});
 };
+
+//show saved movies
+module.exports.showSavedMovies = () => {
+	console.log('showSavedMovies fn');
+	fbFactory.getUserMovies()
+	.then( (userMovieData) => {
+		console.log('userMovieData', userMovieData);
+		module.exports.unwatchedFBToMovieCards(userMovieData);
+	})
+	.catch( (error) => {
+		console.log('error', error);
+	});
+};
+
+// watchedLink
+

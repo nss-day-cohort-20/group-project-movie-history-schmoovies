9516dde14d368e792feca683b8796c21b1016598
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-07-20 13:06:50 -0500

    added actors to template

--- javascripts/movie-view-controller.js
+++ javascripts/movie-view-controller.js
@@ -4,9 +4,20 @@ let $ = require('jquery');
 let Handlebars = require('hbsfy/runtime');
 
 Handlebars.registerPartial( "movieInfoPartial", require('../templates/partials/movie-info.hbs') );
+Handlebars.registerHelper('year', yearHelper);
 let searchCardTemplate = require ('../templates/searchCards.hbs');
+let unwatchedCardsTemplate = require('../templates/unwatchedCard.hbs');
+
+function yearHelper(dateString) {
+	return dateString.slice(0,4);
+}
 
 module.exports.searchDataToMovieCards = (data) => {
 	let cards = searchCardTemplate({movies: data.results});
+	$("#movieContainer").html(cards);
+};
+
+module.exports.unwatchedDataToMovieCards = (data) => {
+	let cards = unwatchedCardsTemplate({movies: data.results});
 	$("#movieContainer").html(cards);
 };
